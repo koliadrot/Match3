e9805d096a6fd6f95e82b888acb4cb64cd954509
koliadrot <koliadrot@mail.ru>
2021-06-07 01:47:59 +0200

    Created GameBoard

--- Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -24,34 +24,39 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class BoardManager : MonoBehaviour {
-	public static BoardManager instance;
-	public List<Sprite> characters = new List<Sprite>();
-	public GameObject tile;
-	public int xSize, ySize;
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance;
+    public List<Sprite> characters = new List<Sprite>();
+    public GameObject tile;
+    public int xSize, ySize;
 
-	private GameObject[,] tiles;
+    private GameObject[,] tiles;
 
-	public bool IsShifting { get; set; }
+    public bool IsShifting { get; set; }
 
-	void Start () {
-		instance = GetComponent<BoardManager>();
+    void Start()
+    {
+        instance = GetComponent<BoardManager>();
 
-		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
     }
 
-	private void CreateBoard (float xOffset, float yOffset) {
-		tiles = new GameObject[xSize, ySize];
+    private void CreateBoard(float xOffset, float yOffset)
+    {
+        tiles = new GameObject[xSize, ySize];
 
         float startX = transform.position.x;
-		float startY = transform.position.y;
+        float startY = transform.position.y;
 
-		for (int x = 0; x < xSize; x++) {
-			for (int y = 0; y < ySize; y++) {
-				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
-				tiles[x, y] = newTile;
-			}
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+                tiles[x, y] = newTile;
+            }
         }
     }
 
