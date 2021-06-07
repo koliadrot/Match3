388d5e6c8625cc3aa9535ab606813eb81b1184a0
koliadrot <koliadrot@mail.ru>
2021-06-07 02:12:35 +0200

    Operation of counter and count game points

--- Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
+++ Assets/Match 3 Starter/Scripts/Managers/GUIManager.cs
@@ -24,36 +24,72 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class GUIManager : MonoBehaviour {
-	public static GUIManager instance;
+public class GUIManager : MonoBehaviour
+{
+    public static GUIManager instance;
 
-	public GameObject gameOverPanel;
-	public Text yourScoreTxt;
-	public Text highScoreTxt;
+    public GameObject gameOverPanel;
+    public Text yourScoreTxt;
+    public Text highScoreTxt;
 
-	public Text scoreTxt;
-	public Text moveCounterTxt;
+    public Text scoreTxt;
+    public Text moveCounterTxt;
 
-	private int score;
+    private int score;
+    private int moveCounter;
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
 
-	void Awake() {
-		instance = GetComponent<GUIManager>();
-	}
+        set
+        {
+            score = value;
+            scoreTxt.text = score.ToString();
+        }
+    }
 
-	// Show the game over panel
-	public void GameOver() {
-		GameManager.instance.gameOver = true;
+    public int MoveCounter
+    {
+        get
+        {
+            return moveCounter;
+        }
 
-		gameOverPanel.SetActive(true);
+        set
+        {
+            moveCounter = value;
+            moveCounterTxt.text = moveCounter.ToString();
+        }
+    }
 
-		if (score > PlayerPrefs.GetInt("HighScore")) {
-			PlayerPrefs.SetInt("HighScore", score);
-			highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
-		} else {
-			highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
-		}
+    void Awake()
+    {
+        instance = GetComponent<GUIManager>();
+        moveCounter = 60;
+        moveCounterTxt.text = moveCounter.ToString();
+    }
 
-		yourScoreTxt.text = score.ToString();
-	}
+    // Show the game over panel
+    public void GameOver()
+    {
+        GameManager.instance.gameOver = true;
+
+        gameOverPanel.SetActive(true);
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highScoreTxt.text = "New Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+        else
+        {
+            highScoreTxt.text = "Best: " + PlayerPrefs.GetInt("HighScore").ToString();
+        }
+
+        yourScoreTxt.text = score.ToString();
+    }
 
 }
